8104d8eeb12d89c13cac5dbd672dfe7c5314df08
eneko-osia <eneko@tuta.com>
2025-06-04 19:19:21 +0200

    add timer block and subblock components

---
+++ components/TimerBlockEditor.tsx
@@ -0,0 +1,127 @@
+// react imports
+import React, { useRef } from 'react';
+import {
+    StyleSheet,
+    Text,
+    TouchableOpacity,
+    useColorScheme,
+    View,
+} from 'react-native';
+import { MaterialIcons } from '@expo/vector-icons';
+import DraggableFlatList, { RenderItemParams } from 'react-native-draggable-flatlist';
+
+// project imports
+import { TimerBlock, TimerSubBlock, Workout } from '../types/workout';
+import { useStyles } from '../styles/common';
+import { useTheme } from '../styles/theme';
+import TimerSubBlockEditor from './TimerSubBlockEditor';
+
+// type definitions
+type Props = {
+    workout: Workout,
+    block: TimerBlock;
+    onChange: () => void;
+};
+
+// component
+export default function TimerBlockEditor({ workout, block, onChange }: Props) {
+    // hooks
+    const scheme = useColorScheme();
+
+    // theme
+    const theme = useTheme(scheme);
+    const style = StyleSheet.create({
+        ...useStyles(theme),
+        fixWidth: { width: theme.spacing.xxl },
+    })
+
+    // attributes
+    const timer = useRef<NodeJS.Timeout>(null);
+
+    // methods
+    const createSubBlock = (blockId: number) => {
+        workout.addSubBlock(blockId, {
+            label: 'New Block',
+            duration: 10
+        });
+        onChange();
+    };
+
+    const deleteBlock = (blockId: number) => {
+        workout.deleteBlock(blockId)
+        onChange();
+    };
+
+    const decreaseSets = (block: TimerBlock, timeout: number = 250) => {
+        block.sets = Math.max(Workout.kMinSets, block.sets - 1)
+        timer.current = setTimeout(() => { decreaseSets(block, Math.max(1, timeout - 10)) }, timeout);
+        onChange();
+    };
+
+    const increaseSets = (block: TimerBlock, timeout: number = 250) => {
+        block.sets = Math.min(Workout.kMaxSets, block.sets + 1)
+        timer.current = setTimeout(() => { increaseSets(block, Math.max(1, timeout - 10)) }, timeout);
+        onChange();
+    };
+
+    const stopTimer = () => {
+        clearTimeout(timer.current!);
+        timer.current = null;
+    }
+
+    // jsx
+    return (
+        <View style = { [ style.tertiary, style.marginTop, style.padding, style.border, style.outline ] } key = { block.id }>
+            <View style = { [ style.tertiary, style.marginHorizontal, style.row ] }>
+                <Text style = { [ style.text, style.normal, style.left, style.marginRight, style.flex1 ] } numberOfLines = { 1 }>
+                    Sets
+                </Text>
+                <TouchableOpacity style = { [ style.quaternary, style.marginVertical, style.padding, style.button, (block.sets <= Workout.kMinSets ?  style.disabled : {}), style.border, style.outline ] }
+                    disabled = { block.sets <= Workout.kMinSets }
+                    onPressIn = { () => { decreaseSets(block); } }
+                    onPressOut = { () => { stopTimer(); } }
+                >
+                    <MaterialIcons name = 'remove' size = { theme.iconSize.sm }/>
+                </TouchableOpacity>
+                <Text style = { [ style.text, style.normal, style.center, style.fixWidth ] }>
+                    { block.sets }
+                </Text>
+                <TouchableOpacity style = { [ style.quaternary, style.marginVertical, style.padding, style.button, (block.sets >= Workout.kMaxSets ?  style.disabled : {}), style.border, style.outline ] }
+                    disabled = { block.sets >= Workout.kMaxSets }
+                    onPress = { () => { block.sets = Math.min(Workout.kMaxSets, block.sets + 1); onChange(); } }
+                    onPressIn = { () => { increaseSets(block); } }
+                    onPressOut = { () => { stopTimer(); } }
+                >
+                    <MaterialIcons name = 'add' size = { theme.iconSize.sm }/>
+                </TouchableOpacity>
+            </View>
+            <TouchableOpacity style = { [ style.quaternary, style.padding, style.button, style.border, style.outline ] }
+                onPress = {() => createSubBlock(block.id)}
+            >
+                <MaterialIcons name = 'add' size = { theme.iconSize.sm }/>
+            </TouchableOpacity>
+            <DraggableFlatList
+                data = { block.subBlocks }
+                keyExtractor = { item => item.id.toString() }
+                onDragEnd = { ({ data }) => { block.subBlocks = data; onChange(); } }
+                scrollEnabled = { false }
+                renderItem = {({ item: subBlock, drag, isActive }: RenderItemParams<TimerSubBlock>) => (
+                    <TouchableOpacity onLongPress = { drag } disabled = { isActive }>
+                        <TimerSubBlockEditor
+                            workout = { workout }
+                            block = { block }
+                            subBlock = { subBlock }
+                            onChange = { onChange }
+                        />
+                    </TouchableOpacity>
+                )}
+            />
+            <TouchableOpacity style = { [ style.quaternary, style.marginTop, style.padding, style.button, (workout.blocks.length <= 1 ?  style.disabled : {}), style.border, style.outline ] }
+                disabled = { workout.blocks.length <= 1 }
+                onPress = { () => { deleteBlock(block.id); } }
+            >
+                <MaterialIcons name = 'remove-circle' size = { theme.iconSize.sm }/>
+            </TouchableOpacity>
+        </View>
+    );
+}
---
+++ components/TimerSubBlockEditor.tsx
@@ -0,0 +1,100 @@
+// react imports
+import React, { useRef } from 'react';
+import {
+	StyleSheet,
+	Text,
+	TextInput,
+	TouchableOpacity,
+	useColorScheme,
+	View,
+} from 'react-native';
+import { MaterialIcons } from '@expo/vector-icons';
+
+// project imports
+import { formatDuration } from '../utils/format';
+import { TimerBlock, TimerSubBlock, Workout } from '../types/workout';
+import { useStyles } from '../styles/common';
+import { useTheme } from '../styles/theme';
+
+// type definitions
+type Props = {
+    workout: Workout,
+    block: TimerBlock;
+    subBlock: TimerSubBlock;
+    onChange: () => void;
+};
+
+// component
+export default function TimerSubBlockEditor({ workout, block, subBlock, onChange }: Props) {
+    // hooks
+    const scheme = useColorScheme();
+
+    // theme
+    const theme = useTheme(scheme);
+    const style = StyleSheet.create({
+        ...useStyles(theme),
+        fixWidth: { width: theme.spacing.xxl },
+    })
+
+    // attributes
+    const timer = useRef<NodeJS.Timeout>(null);
+
+    // methods
+    const deleteSubBlock = (blockId: number, subBlockId: number) => {
+        workout.deleteSubBlock(blockId, subBlockId);
+        onChange();
+    };
+
+    const decreaseDuration = (blockId: number, subBlock: TimerSubBlock, timeout: number = 250) => {
+        subBlock.duration = Math.max(Workout.kMinDuration, subBlock.duration - 1)
+        timer.current = setTimeout(() => { decreaseDuration(blockId, subBlock, Math.max(1, timeout - 10)) }, timeout);
+        onChange();
+    };
+
+    const increaseDuration = (blockId: number, subBlock: TimerSubBlock, timeout: number = 250) => {
+        subBlock.duration = Math.min(Workout.kMaxDuration, subBlock.duration + 1)
+        timer.current = setTimeout(() => { increaseDuration(blockId, subBlock, Math.max(1, timeout - 10)) }, timeout);
+        onChange();
+    };
+
+    const stopTimer = () => {
+        clearTimeout(timer.current!);
+        timer.current = null;
+    }
+
+    // jsx
+    return (
+        <View style = { [ style.secondary, style.marginTop, style.padding, style.border, style.outline ] } key = { subBlock.id }>
+            <View style = { [ style.secondary, style.row ] }>
+                <TextInput style = { [ style.text, style.input, style.normal, style.left, style.marginRight, style.padding, style.flex1  ] }
+                    value = { subBlock.label }
+                    maxLength = { 64 }
+                    onChangeText = {(text) => { subBlock.label = text; onChange(); } }
+                />
+                <TouchableOpacity style = { [ style.quaternary, style.marginVertical, style.padding, style.button, (subBlock.duration <= Workout.kMinDuration ?  style.disabled : {}), style.border, style.outline ] }
+                    disabled = { subBlock.duration <= Workout.kMinDuration }
+                    onPressIn = { () => { decreaseDuration(block.id, subBlock); } }
+                    onPressOut = { () => { stopTimer(); } }
+                >
+                    <MaterialIcons name = 'remove' size = { theme.iconSize.sm }/>
+                </TouchableOpacity>
+                <Text style = { [ style.text, style.normal, style.center, style.fixWidth ] }>
+                    { formatDuration(subBlock.duration) }
+                </Text>
+                <TouchableOpacity style = { [ style.quaternary, style.marginVertical, style.padding, style.button, (subBlock.duration >= Workout.kMaxDuration ?  style.disabled : {}), style.border, style.outline ] }
+                    disabled = { subBlock.duration >= Workout.kMaxDuration }
+                    onPressIn = { () => { increaseDuration(block.id, subBlock); } }
+                    onPressOut = { () => { stopTimer(); } }
+                >
+                    <MaterialIcons name = 'add' size = { theme.iconSize.sm }/>
+                </TouchableOpacity>
+            </View>
+            <TouchableOpacity style = { [ style.quaternary, style.padding, style.button, (block.subBlocks.length <= 1 ?  style.disabled : {}), style.border, style.outline ] }
+                disabled = { block.subBlocks.length <= 1 }
+                onPress = { () => { deleteSubBlock(block.id, subBlock.id); } }
+            >
+                <MaterialIcons name = 'delete' size = { theme.iconSize.sm }/>
+            </TouchableOpacity>
+        </View>
+    );
+}
