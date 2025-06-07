06731f5f4de421141114843fd0810eb998e97a0c
eneko-osia <eneko@tuta.com>
2025-06-07 11:08:08 +0200

    dark theme colors updated

--- styles/theme.ts
+++ styles/theme.ts
@@ -42,10 +42,10 @@ export const dark = {
     ...DarkTheme,
     colors: {
         ...DarkTheme.colors,
-        primary:    'rgb(90, 130, 126)',
-        secondary:  'rgb(132, 174, 146)',
-        tertiary:   'rgb(185, 212, 170)',
-        quaternary: 'rgb(250, 255, 202)',
+        primary:    'rgb(30, 32, 30)',
+        secondary:  'rgb(60, 61, 55)',
+        tertiary:   'rgb(105, 117, 101)',
+        quaternary: 'rgb(236, 223, 204)',
         input:      'rgb(18, 18, 18)',
     },
 };
