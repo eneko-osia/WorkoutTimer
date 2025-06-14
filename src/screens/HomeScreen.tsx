9777868c1ee285ce47f57337222612ec11e4b2fd
eneko-osia <eneko@tuta.com>
2025-06-14 21:48:09 +0200

    update HomeScreen to display workouts and add navigation

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -9,19 +9,19 @@ import {
     useColorScheme,
     View,
 } from 'react-native';
-// import { MaterialIcons } from '@expo/vector-icons';
 import { NativeStackNavigationProp } from '@react-navigation/native-stack';
 import { useFocusEffect, useNavigation } from '@react-navigation/native';
-// import DraggableFlatList, { RenderItemParams } from 'react-native-draggable-flatlist';
+import DraggableFlatList, { RenderItemParams } from 'react-native-draggable-flatlist';
+import MaterialIcons from '@react-native-vector-icons/material-icons';
 
 // project imports
-// import { deleteWorkout, loadWorkouts, saveWorkouts } from '../utils/storage';
-// import { formatDuration } from '../utils/format';
-// import { generateId } from '../utils/id';
+import { deleteWorkout, loadWorkouts, saveWorkouts } from '../utils/storage';
+import { formatDuration } from '../utils/format';
+import { generateId } from '../utils/id';
 import { RootStackParamList } from '../navigation/types';
 import { useStyles } from '../styles/common';
 import { useTheme } from '../styles/theme';
-// import { Workout } from '../types/workout';
+import { Workout } from '../types/workout';
 
 // type definitions
 type HomeScreenNavigationProp = NativeStackNavigationProp<RootStackParamList, 'Home'>;
@@ -36,6 +36,63 @@ export default function HomeScreen() {
     const theme = useTheme(scheme);
     const style = StyleSheet.create({ ...useStyles(theme) })
 
+    // attributes
+    const [ workouts, setWorkouts ] = useState<Workout[]>([]);
+
+    // effects
+    useFocusEffect(
+        useCallback(() => {
+            loadAsync();
+        }, [])
+    );
+
+    // methods
+    const createWorkout = () => {
+        const workout: Workout = new Workout({ id: generateId(), name: 'New Workout' });
+        {
+            const blockId: number = workout.createBlock();
+            workout.createSubBlock(blockId, 'Prepare', 5);
+        }
+        {
+            const blockId: number = workout.createBlock();
+            workout.createSubBlock(blockId, 'Work', 10);
+            workout.createSubBlock(blockId, 'Rest', 10);
+        }
+        {
+            const blockId: number = workout.createBlock();
+            workout.createSubBlock(blockId, 'Cooldown', 30);
+        }
+        return workout;
+    }
+
+    const removeWorkout = (workout: Workout) => {
+        if (Platform.OS === 'web') {
+            deleteAsync(workout.id);
+        }
+        else {
+            Alert.alert(
+            'Delete Workout',
+            `Are you sure you want to delete "${workout.name}"?`,
+            [
+                { text: 'Cancel', style: 'cancel' },
+                { text: 'Delete', style: 'destructive', onPress: () => { deleteAsync(workout.id); } }
+            ]);
+        }
+    }
+
+    const deleteAsync = async (id: number) => {
+        await deleteWorkout(Workout.kStorageKey, id);
+        loadAsync();
+    }
+
+    const loadAsync = async () => {
+        setWorkouts(await loadWorkouts(Workout.kStorageKey));
+    };
+
+    const saveAsync = async (workouts: Workout[]) => {
+        await saveWorkouts(Workout.kStorageKey, workouts);
+    };
+
     // jsx
     return (
         <View style = { [ style.containerPrimary ] }>
@@ -43,11 +100,63 @@ export default function HomeScreen() {
                 <Text style = { [ style.text, style.large, style.bold, style.left ] } numberOfLines = { 1 }>
                     Workouts
                 </Text>
-                <TouchableOpacity style = { [ style.quaternary, style.padding, style.button, style.border, style.outline ] }>
-                    {/* <MaterialIcons name = 'add' size = { theme.sizes.sm }/> */}
-                    <Text>+</Text>
+                <TouchableOpacity style = { [ style.quaternary, style.padding, style.button, style.border, style.outline ] }
+                    onPress = { () => { navigation.navigate('Setup', { workout: createWorkout(), pendingSave: true }); } }
+                >
+                    <MaterialIcons name = 'add' size = { theme.sizes.sm }/>
                 </TouchableOpacity>
             </View>
+            {workouts.length === 0 ? (
+                <></>
+            ) : (
+                <View style = { [ style.secondary, style.margin, style.padding, style.border, style.outlineThick, style.flex1 ] }>
+                    <DraggableFlatList
+                        data = { workouts }
+                        keyExtractor = { item => item.id.toString() }
+                        onDragEnd = { ({ data }) => { setWorkouts(data); saveAsync(data); } }
+                        scrollEnabled = { true }
+                        renderItem = {({ item: workout, drag, isActive }: RenderItemParams<Workout>) => (
+                            <View style = { [ style.tertiary, style.marginTop, style.padding, style.border, style.outlineThick ] } key = { workout.id }>
+                                {/* <Text style = { [ style.text, style.normal, style.bold ] }>
+                                    { workout.id }
+                                </Text> */}
+                                <View style = { [ style.tertiary, style.marginHorizontal, style.row ] }>
+                                    <Text style = { [ style.text, style.normal, style.bold, style.left, style.flex1 ] } numberOfLines = { 1 }>
+                                        { workout.name }
+                                    </Text>
+                                    <Text style = { [ style.text, style.normal, style.bold, style.right ] } numberOfLines = { 1 }>
+                                        { formatDuration(workout.totalDuration) }
+                                    </Text>
+                                </View>
+                                <View style = { [ style.tertiary, style.marginTop, style.row ] }>
+                                    <TouchableOpacity style = { [ style.quaternary, style.padding, style.button, (workout.blocks.length === 0 ?  style.disabled : {}), style.border, style.outline, style.flex1 ] }
+                                        disabled = { workout.blocks.length === 0 }
+                                        onPress = { () => { navigation.navigate('Timer', { workout }); }}
+                                    >
+                                        <MaterialIcons name = 'play-arrow' size = { theme.sizes.sm }/>
+                                    </TouchableOpacity>
+                                    <TouchableOpacity style = { [ style.quaternary, style.marginLeft, style.padding, style.button, style.border, style.outline ] }
+                                        onPress = { () => { navigation.navigate('Setup', { workout: workout, pendingSave: false }); }}
+                                    >
+                                        <MaterialIcons name = 'edit' size = { theme.sizes.sm }/>
+                                    </TouchableOpacity>
+                                    <TouchableOpacity style = { [ style.quaternary, style.marginLeft, style.padding, style.button, style.border, style.outline ] }
+                                        onPress = { () => { removeWorkout(workout); } }
+                                    >
+                                        <MaterialIcons name = 'delete' size = { theme.sizes.sm }/>
+                                    </TouchableOpacity>
+                                    <TouchableOpacity style = { [ style.padding ] }
+                                        disabled = { isActive }
+                                        onPressOut = { drag }
+                                    >
+                                        <MaterialIcons name = 'reorder' size = { theme.sizes.sm }/>
+                                    </TouchableOpacity>
+                                </View>
+                            </View>
+                        )}
+                    />
+                </View>
+            )}
         </View>
     );
 }
