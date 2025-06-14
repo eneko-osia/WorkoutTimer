5f9ccfec4bd5e42598a25d8684776c34df1fe3e9
eneko-osia <eneko@tuta.com>
2025-06-14 13:01:03 +0200

    navigation folder added

---
+++ src/navigation/types.ts
@@ -0,0 +1,7 @@
+// import { Workout } from '../types/workout';
+
+export type RootStackParamList = {
+    Home: undefined;
+    // Setup: { workout: Workout, pendingSave: boolean };
+    // Timer: { workout: Workout };
+};
