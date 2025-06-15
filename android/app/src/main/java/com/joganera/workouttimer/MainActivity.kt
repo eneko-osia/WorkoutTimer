b488c47c4753874c8fc5f6071aa2eb54ef20aeab
eneko-osia <eneko@tuta.com>
2025-06-15 11:51:18 +0200

    package name updated

--- android/app/src/main/java/com/joganera/workouttimer/MainActivity.kt
+++ android/app/src/main/java/com/joganera/workouttimer/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.workouttimer
+package com.joganera.workouttimer
 
 import android.os.Bundle;
 import com.facebook.react.ReactActivity
--- android/app/src/main/java/com/joganera/workouttimer/MainApplication.kt
+++ android/app/src/main/java/com/joganera/workouttimer/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.workouttimer
+package com.joganera.workouttimer
 
 import android.app.Application
 import com.facebook.react.PackageList
